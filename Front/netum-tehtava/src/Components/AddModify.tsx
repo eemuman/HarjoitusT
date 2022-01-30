c01f8a6656c92e4df97d82b93aba5291d2a9bf8a
Eemil V <eemilv11@gmail.com>
2022-01-30 15:41:46 +0200

    Lisätty uuden / vanhan muokkaamista varten Elementti

---
+++ Front/netum-tehtava/src/Components/AddModify.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+export default function AddModify() {
+  const [show, setShow] = useState<Boolean>(false);
+  const [newUser, setNewUser] = useState<any>({
+    fName: "",
+    lName: "",
+    age: 0,
+  });
+
+  const handleShow = () => {
+    setShow(true);
+  };
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewUser({ ...newUser, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = () => {
+    console.log(newUser);
+    setNewUser({ fName: "", lName: "", age: 0 });
+    handleClose();
+  };
+
+  return show ? (
+    <form onSubmit={handleSubmit}>
+      <h2>LISÄÄ / MUOKKAA TIETOJA</h2>
+      <label>
+        Etunimi
+        <input
+          type="text"
+          id="fName"
+          name="fName"
+          placeholder="Etunimi"
+          value={newUser.fName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Sukunimi
+        <input
+          type="text"
+          id="lName"
+          name="lName"
+          placeholder="Sukunimi"
+          value={newUser.lName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Ikä
+        <input
+          type="number"
+          min="0"
+          max="100"
+          id="age"
+          name="age"
+          placeholder="Ikä"
+          value={newUser.age}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" value="Tallenna" />
+    </form>
+  ) : (
+    <button onClick={handleShow}>TESTBUTTON</button>
+  );
+}
