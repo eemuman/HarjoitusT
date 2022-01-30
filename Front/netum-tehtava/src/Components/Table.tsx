5357b2a37222c6a46bf0e9ee100cf87f6d1a4633
Eemil V <eemilv11@gmail.com>
2022-01-30 18:53:54 +0200

    Muutama pieni muokkaus, tablen shadow pienemmäksi ja otetaan propsit const data variableen

--- Front/netum-tehtava/src/Components/Table.tsx
+++ Front/netum-tehtava/src/Components/Table.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { userProps } from "./UserInterface";
 
 export default function Table(props: userProps) {
+  const data = props.curUsers;
+
   return (
     <div className="TableBox">
       <table>
@@ -13,8 +15,8 @@ export default function Table(props: userProps) {
           </tr>
         </thead>
         <tbody>
-          {props.curUsers.map((user) => (
-            <tr>
+          {data.map((user) => (
+            <tr key={user.fName}>
               <td>{user.fName}</td>
               <td>{user.lName}</td>
               <td>{user.age}</td>
