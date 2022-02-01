ff796273f1364e5c80b232210bc36867f9f512b9
Eemil V <eemilv11@gmail.com>
2022-02-01 14:08:21 +0200

    Siirretty fetchaus omaan funktion, lisätty myös uuden postaus

--- Front/netum-tehtava/src/App.tsx
+++ Front/netum-tehtava/src/App.tsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import AddModify from "./Components/AddNew";
-import { getAll } from "./Components/FetchUtils";
+import { getAll, postNew } from "./Components/FetchUtils";
 import Table from "./Components/Table";
 import { user } from "./Components/UserInterface";
 
 function App() {
   const [curUsers, setCurUsers] = useState<Array<user>>([]);
 
+  const fetchAll = async () => {
+    const data = await getAll("http://localhost:8000/users");
+    setCurUsers(data);
+  };
+
   useEffect(() => {
-    getAll("http://localhost:8000/users").then((res) => setCurUsers(res));
+    fetchAll();
   }, []);
 
-  const addNewUser = (user: user) => {
-    setCurUsers([...curUsers, user]);
+  const addNewUser = async (user: user) => {
+    await postNew("http://localhost:8000/users", user);
+    await fetchAll();
   };
 
   return (
