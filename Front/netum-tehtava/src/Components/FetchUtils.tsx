33e5b922fb381b832c9af64adbb6d9847f323838
Eemil V <eemilv11@gmail.com>
2022-02-01 14:08:47 +0200

    Uuden postaamiseen funktio

--- Front/netum-tehtava/src/Components/FetchUtils.tsx
+++ Front/netum-tehtava/src/Components/FetchUtils.tsx
@@ -1,3 +1,29 @@
-export function getAll(url: string) {
-  return fetch(url).then((res) => res.json());
+import { user } from "./UserInterface";
+
+export async function getAll(url: string) {
+  try {
+    const resp = await fetch(url);
+    return await resp.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function postNew(url: string, data: user) {
+  try {
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fName: data.fName,
+        lName: data.lName,
+        age: data.age,
+      }),
+    });
+    const rsp = await resp.json();
+    console.log(rsp);
+    return rsp;
+  } catch (err) {}
 }
