f5cbd12d980aff7a14da3c5f6595c9364af04703
Eemil V <eemilv11@gmail.com>
2022-02-01 11:52:55 +0200

    Lisätty pohja APIlle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-function sum(num1: number, num2: number) {
-  return num1 + num2;
-}
-console.log(sum(8, 4));
+import express, {
+  Request,
+  Response,
+  Application,
+  request,
+  response,
+} from "express";
+require("dotenv").config();
+const server = require("./Server");
+
+const app: Application = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log("Yhteys upis");
+});
+
+app.use(express.json());
+
+app.get(`/users`, async (req: Request, res: Response) => {
+  const data = await server.getAll();
+  res.send(data);
+});
+
+app.patch(`/users`, async (req: Request, res: Response) => {
+  const data = await server.updateById(req.params.id, req.body);
+  res.send(data);
+});
