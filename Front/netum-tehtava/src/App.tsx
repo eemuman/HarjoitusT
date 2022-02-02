c1ffb65c737d1a2dfe0ca795a59fb939066a5812
Eemil V <eemilv11@gmail.com>
2022-02-02 16:50:23 +0200

    Viimeistelyä

--- Front/netum-tehtava/src/App.tsx
+++ Front/netum-tehtava/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import AddModify from "./Components/AddNew";
-import { getAll, postNew } from "./Components/FetchUtils";
+import { getAll } from "./Components/FetchUtils";
 import Table from "./Components/Table";
 import { user } from "./Components/UserInterface";
 
